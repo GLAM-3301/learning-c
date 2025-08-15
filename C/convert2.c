9b3859c99a5e64b948f20208f904c9a93a10244e
GLAM-3301 <glayst.3301@gmail.com>
2025-08-15 18:16:55 +0500

    Delete C/convert2.c

--- C/convert2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void usage(char *program_name) {
-	printf("Usage: %s <message> <# of times to repeat>\n", program_name);
-	exit(1);
-}
-
-int main(int argc, char *argv[]) {
-	int i, count;
